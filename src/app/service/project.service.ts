545a9aa4926e8644ab0766e99c3f3206a04ac97c
kyasuko <lychnussys@gmail.com>
2020-08-19 19:03:45 +0800

    fix: add unsubscribe
    Just add it for fun

--- src/app/service/project.service.ts
+++ src/app/service/project.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { callbackify } from 'util';
 
 @Injectable({
   providedIn: 'root'
@@ -12,19 +11,20 @@ export class ProjectService {
   private valueChanges: Observable<any>
   private projects: any;
   constructor(private router: Router, private firestore: AngularFirestore) {
-    this.valueChanges = this.firestore.collection('projects', ref => ref.orderBy('order'))
+    this.valueChanges = this.firestore
+      .collection('projects', ref => ref.orderBy('order'))
       .valueChanges({ idField: 'id' })
   }
 
   init(component) {
     if (!this.projects) {
-      this.valueChanges.subscribe((p: any) => {
+      let subscription = this.valueChanges.subscribe((p: any) => {
         this.setProjects(p);
         component.setData();
+        subscription.unsubscribe();
       });
     } else {
       component.setData();
-      
     }
   }
 
