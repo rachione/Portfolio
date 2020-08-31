70a1d40835036534c24ee1cc659ef366bef2c3cf
rachione <lychnussys@gmail.com>
2020-08-31 13:59:08 +0900

    fix: pdf
    fix

--- src/app/page/resume/resume.component.ts
+++ src/app/page/resume/resume.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent implements OnInit {
-  pdfSrc = '/assets/Resume.pdf'
+  pdfSrc = 'assets/Resume.pdf'
   constructor() { }
 
   ngOnInit(): void {
